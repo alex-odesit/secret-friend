56de18dcfad7b32b76a3de5243b3118896ce44e4
Alex Dmitriev <a.dmitriev@gamzix.com>
2022-11-21 23:43:52 +0200

    ready for json functional

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
 const receiveBtn = document.getElementById('receive_btn');
 const result = document.getElementById('result');
-let sender = document.getElementById('sender');
-let receiver = document.getElementById('receiver');
+const sender = document.getElementById('sender');
+const receiver = document.getElementById('receiver');
+const passwordInput = document.getElementById('password');
+const input_wrapper = document.getElementById('input_wrapper');
+const PromisePlayers = fetch('./js/result.json').then(res => res.json());
 
-let test = window.btoa('alex');
-console.log(test)
-console.log(window.atob('QWxleA=='))
+// let test = window.btoa('alex');
+// console.log(test)
+// console.log(window.atob('QW55YQ=='))
 
 receiveBtn.addEventListener('click', receiveReceiver)
 
-function receiveReceiver(e){
+async function receiveReceiver(e){
+    if(!passwordInput.value) return;
+    let players = await PromisePlayers;
+    let player = players[passwordInput.value];
+    if(!player){
+        input_wrapper.classList.add('red')
+        return;
+    }
+
     receiveBtn.classList.add('opacityHide');
     result.classList.add('d-flex');
-    sender.textContent = 'Alex';
-    receiver.textContent = 'Daniel';
+    sender.textContent = window.atob(player.gives);
+    receiver.textContent = player.receives;
 
     setTimeout(()=>{
         receiveBtn.classList.add('visibilityHidden');
